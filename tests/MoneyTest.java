86918ac5ba4d8bbb73f30e6c909dccafbc0ac137
Nicolas HADDAD <nicolas.haddad80@gmail.com>
2020-02-16 09:08:54 +0100

    Exercice3 Complete

--- tests/MoneyTest.java
+++ tests/MoneyTest.java
@@ -3,13 +3,26 @@ package fr.cnam.tp11.tests;
 import fr.cnam.tp11.Money;
 
 public class MoneyTest {
+  
+    private Money m1;
+    private Money m2;
 
-    public static void main(String[] Args) {
-        Money Money1 = new Money(5, "Euro");
-        Money Money2 = new Money(10, "Euro");
-        Money Money3 = new Money(20, "Dollars");
+    public void setUp() {
+        this.m1 = new Money(5, "Euros");
+        this.m2 = new Money(7, "Euros");
+    }
 
-        Money1.add(Money2);
-        Money1.add(Money3);
+    public void testAdd() {
+        m1.add(m2);
+        Assert.assertTrue(12 == m1.getAmount());
+    }
+
+    public void testSub() {
+        m1.sub(m2);
+        Assert.assertTrue(-2== m1.getAmount());
+    }
+
+    public void tearDown() {
+
     }
 }
